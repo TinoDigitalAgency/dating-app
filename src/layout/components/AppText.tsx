e818240ad6e14f1679c0275c1cd75e6f5cddbfd6
Tino <tino.agency@gmail.com>
2023-02-17 15:10:11 +0200

    waitlist screen

---
+++ src/layout/components/AppText.tsx
@@ -0,0 +1,35 @@
+import React, {ReactNode} from "react";
+import {StyleSheet, Text, TextStyle} from "react-native";
+import {THEME} from "../../utils/theme";
+
+type TextProps = {
+  bottom?: number,
+  top?: number
+  styles?: TextStyle,
+  center?: boolean,
+  children: ReactNode
+}
+
+export const AppTitle = (props:TextProps) => {
+  return (
+    <Text style={[
+      styles.text,
+      props.top ? {marginTop: props.top} : null,
+      props.bottom ? {marginBottom: props.bottom} : null,
+      props.center ? {textAlign: 'center'} : null,
+      props.styles
+    ]}
+    >
+      {props.children}
+    </Text>
+  )
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: 'Poppins-bold',
+    fontSize: 22,
+    lineHeight: 33,
+    color: THEME.PRIMARY_COLOR
+  }
+});
